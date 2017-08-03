aa5c25ca323b2ca4baeaa42512b8a55a932ff584
Juan Manuel <juanito.lg@gmail.com>
2017-08-03 13:47:59 -0600

    Add a event in button for Create new employee

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { employeeUpdate } from '../actions';
+import { employeeUpdate, employeeCreate } from '../actions';
 import { Picker, Text } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
+
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   render() {
     return (
       <Card>
@@ -43,7 +49,7 @@ class EmployeeCreate extends Component {
         </CardSection>
 
         <CardSection>
-          <Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Create
           </Button>
         </CardSection>
@@ -56,7 +62,8 @@ class EmployeeCreate extends Component {
 const styles = {
   pickerTextStyle: {
     fontSize: 18,
-    paddingLeft:20
+    paddingLeft:20,
+    paddingTop:10
   }
 }
 
@@ -65,4 +72,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift};
 }
 
-export default connect (mapStateToProps, { employeeUpdate }) (EmployeeCreate);
+export default connect (mapStateToProps, { employeeUpdate, employeeCreate }) (EmployeeCreate);
