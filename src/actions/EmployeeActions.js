bb2e4978a17d528b18cad29398d23c7874b4d1c9
Juan Manuel <juanito.lg@gmail.com>
2017-08-02 18:01:47 -0600

    Add Form, Action and Reducer Employee

---
+++ src/actions/EmployeeActions.js
@@ -0,0 +1,9 @@
+//Action creator
+import { EMPLOYEE_UPDATE } from './types';
+
+export const employeeUpdate = ({ prop, value }) => {
+  return {
+    type: EMPLOYEE_UPDATE,
+    payload: { prop, value }
+  };
+};
--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form </Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input
+            label:"Name"
+            placeholder:"Juan"
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            label:"Phone"
+            placeholder:"667-37-2334"
+          />
+        </CardSection>
+
+        <CardSection>
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Create
+          </Button>
+        </CardSection>
+
+      </Card>
     );
   }
 }
