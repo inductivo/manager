d1d229daebd71688de1fff336663b7b769b775ed
Juan Manuel <juanito.lg@gmail.com>
2017-08-04 16:17:38 -0600

    Add Lodash for convert arrays in objetcs

--- src/actions/EmployeeActions.js
+++ src/actions/EmployeeActions.js
@@ -1,7 +1,7 @@
 //Action creator
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS } from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -21,4 +21,14 @@ export const employeeCreate = ({ name, phone, shift }) => {
           Actions.employeeList();
         });
     };
+};
+
+export const employeesFetch = () => {
+  const { currentUser } = firebase.auth();
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .on('value', snapshot => {
+      dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
+    });
+  };
 };
