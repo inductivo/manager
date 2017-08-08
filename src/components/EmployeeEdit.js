dfbb76521936957814103f2a3f17969b14a8e03e
Juan Manuel <juanito.lg@gmail.com>
2017-08-07 19:09:28 -0600

    Delete records in firebase

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { Card, CardSection, Button, Confirm } from './common';
 
 class EmployeeEdit extends Component {
@@ -24,6 +24,16 @@ class EmployeeEdit extends Component {
   onTextPress() {
     const { phone, shift } = this.props;
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onAccept(){
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({ uid });
+    this.setState({ showModal: false });
+  }
+
+  onDecline() {
+    this.setState({ showModal: false });
   }
 
   render() {
@@ -50,6 +60,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -64,4 +76,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave }) (EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete }) (EmployeeEdit);
